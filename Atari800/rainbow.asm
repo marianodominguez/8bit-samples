e7d9d99e6716e56af05ea37c65e2ccb5a572da51
Mariano DM <mariano.dm@gmail.com>
2016-01-05 23:42:38 -0800

    Add rainbow and equates

---
+++ Atari800/rainbow.asm
@@ -0,0 +1,19 @@
+; WUDSN IDE Atari Rainbow Example, add labels
+SDMCTL = $022F ;Disable screen DMA 
+VCOUNT = $D40B
+WSYNC  = $D40A
+COLOR4 = $D01A
+CONSOL = $D01F  ;Function-Keys
+
+      ORG $4000 ;Start of code 
+
+start LDA #0 ;Disable screen DMA 
+      STA SDMCTL
+loop  LDA VCOUNT ;Load VCOUNT 
+      CLC 
+      ADC 20 ;Add counter 
+      STA WSYNC 
+      STA COLOR4 ;Change background color 
+      JMP loop 
+
+      run start ;Define run address
