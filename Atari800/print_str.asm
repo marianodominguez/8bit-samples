83a76e123a5d6f58b0a2adbd4042ec2fdac33c19
Mariano DM <mariano.dm@gmail.com>
2016-01-08 21:44:02 -0800

    add hello world ex

---
+++ Atari800/print_str.asm
@@ -0,0 +1,15 @@
+SAVMSC 	= $58
+MAXLEN	= 100
+		org $0600
+start	ldy #0
+		lda #0
+loop	lda string,Y
+		cmp #$FF
+		beq pause
+		sta (SAVMSC),Y
+		iny
+		cpy MAXLEN
+		bne loop
+pause 	jmp pause
+string	.byte $0,$40,"Hello 6502 World !",$40,$FF
+		.byte "please terminate your strings with $FF",$FF
