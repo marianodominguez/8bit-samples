375f51c1387b42aae8e60e3e8e1e98e61aecd49a
Mariano DM <mariano.dm@gmail.com>
2016-01-10 21:27:21 -0800

    add print

--- Atari800/print_str.asm
+++ Atari800/print_str.asm
@@ -1,8 +1,9 @@
-SAVMSC 	= $58
-MAXLEN	= 100
-		org $0600
-start	ldy #0
-		lda #0
+SAVMSC 	=	$58
+MAXLEN 	=	100
+  		org	$0600
+  		
+start 	ldy #0
+  		lda #0
 loop	lda string,Y
 		cmp #$FF
 		beq pause
@@ -11,5 +12,5 @@ loop	lda string,Y
 		cpy MAXLEN
 		bne loop
 pause 	jmp pause
-string	.byte $0,$40,"  Hello 6502 World !  ",$40,$FF
-		.byte "please terminate your strings with $FF",$FF
+string 	.byte $0,$40,"  Hello 6502 World !  ",$40,$FF
+ 		.byte "please terminate your strings with $FF",$FF
