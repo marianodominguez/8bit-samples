e67f3189ba09ca87706dc2b39bad0689cbd8af39
Mariano Dominguez <mariano_molina@apple.com>
2016-01-12 10:48:13 -0800

    add label for rtclock

--- Atari800/rainbow.asm
+++ Atari800/rainbow.asm
@@ -3,6 +3,7 @@ SDMCTL = $022F ;Disable screen DMA
 VCOUNT = $D40B
 WSYNC  = $D40A
 COLOR4 = $D01A
+RTCLOK = $14
 
       ORG $4000 ;Start of code 
 
@@ -10,7 +11,7 @@ start LDA #0 ;Disable screen DMA
       STA SDMCTL
 loop  LDA VCOUNT ;Load VCOUNT 
       CLC 
-      ADC 20 ;Add counter 
+      ADC RTCLOK ;Add counter 
       STA WSYNC 
       STA COLOR4 ;Change background color 
       JMP loop 
