4b97966e64cbe944d3aa047e01d8fb7f5dd53035
Mariano DM <mariano.dm@gmail.com>
2016-07-30 23:27:41 -0700

    remove macros

---
+++ Atari800/antic_no_macros.asm
@@ -0,0 +1,36 @@
+    opt l+h+f-
+    icl 'hardware.asm'
+
+main equ $2000
+scr equ $3010
+
+    org main
+    sei
+    lda #0
+    sta IRQEN
+    sta NMIEN
+    sta DMACTL
+    ;cmp:rne VCOUNT
+wait 
+	cmp VCOUNT
+    bne wait
+    
+    ;mwa #dlist DLISTL
+	lda #dlist&255
+	sta DLISTL
+	lda #dlist/256
+	sta DLISTH
+    mva #$22 DMACTL
+    jmp *
+
+dlist
+    :2 dta $70
+    dta $4E,a(scr)
+    :101 dta $E
+    dta $4E,a(scr+4096-16)
+    :97 dta $E
+    dta $41,a(dlist)
+
+    org scr
+    :8016 dta #&$FF
+    run main
