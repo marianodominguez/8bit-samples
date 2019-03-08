7cc66e9b668a7181e0789fa3c1f548267527f524
Mariano Dominguez <mariano.dm@gmail.com>
2019-03-07 20:55:27 -0800

    update modem phonebook

--- fast_modem.sh
+++ fast_modem.sh
@@ -8,5 +8,6 @@ tcpser -d /dev/ttyUSB0 -s 19200 -l7 -i "&k0" -n l29=bbs.fozztexx.com:6502 \
 -n hawaii=atari-bbs.zapto.org:8888 -n boot=bfbbs.no-ip.com:8888 \
 -n thecafe=thecafe.dtdns.net:8888 -n yyz=yyzbbs.no-ip.org:65 \
 -n 8bg=8bitguild.no-ip.org:130 -n cyber=CYBERSERV.ORG:8005 \
--n irata=IRATA.ONLINE:8005
+-n irata=IRATA.ONLINE:8005 -n bates=thebrewingacademy.com:8888 \
+-n starfleet=bbs.sfhqbbs.org:5983
 
