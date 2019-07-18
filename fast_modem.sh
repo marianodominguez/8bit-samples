305eaabaf574fe4ed385082a0a91119ab7abfd26
Mariano Dominguez <marianod@wepay.com>
2019-07-17 18:03:32 -0700

    fast modem restore

--- fast_modem.sh
+++ fast_modem.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tcpser -d /dev/ttyUSB0 -s 2400 -l7 -i "&k0" -n l29=bbs.fozztexx.com:6502 \
+tcpser -d /dev/ttyUSB0 -s 19200 -l7 -i "&k0" -n l29=bbs.fozztexx.com:6502 \
 -n veteran=bbs.sfhqbbs.org:3523 -n resist=broadway1.lorexddns.net:23 \
 -n alcatraz=alcatrazbbs.ddns.net:9000 -n cth=cth.dtdns.net \
 -n ptime=ptbbs.ddns.net:8000 -n darkforce=darkforce-bbs.dyndns.org:520 \
@@ -10,4 +10,3 @@ tcpser -d /dev/ttyUSB0 -s 2400 -l7 -i "&k0" -n l29=bbs.fozztexx.com:6502 \
 -n 8bg=8bitguild.no-ip.org:130 -n cyber=CYBERSERV.ORG:8005 \
 -n irata=IRATA.ONLINE:8005 -n bates=bbs.thebrewingacademy.com:8888 \
 -n starfleet=bbs.sfhqbbs.org:5983
-
