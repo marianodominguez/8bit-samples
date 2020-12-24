a8da01b93efc4f87db7749abb7ce36d71b671a04
Mariano Dominguez <mariano.dm@gmail.com>
2020-12-23 18:19:53 -0800

    backup all loops

--- Atari800/xor.asm
+++ Atari800/xor.asm
@@ -1,15 +1,35 @@
-;LDA #9
-;JSR $EF9C ;graphics mode
-LDA $40
-STA $026F ; gr. 9
-LDX 0
-L LDA #96
-JSR X
-C JSR $F1D8 ; call OS PLOT
+  org $4000
+  LDA #9 ; gr mode
+  JSR $EF9C ;graphics mode
+  
+  LDX #0
+  LDA #0
+  STA 0
+  STA 2
+  LDY #14 ;pixel lum
+  
+L LDA 2 
+  STA 84 ;row
+  LDA 0 
+  STA 85 ;col low
 
-STY 763 ; color
-X ADC 0,X
-STA 0,X
-INX
-RTS
-.END
+  STY 763 ; color reg
+  JSR X
+C JSR $F1D8 ; call OS PLOT
+  JMP L
+
+X INX
+  STX 0
+  CPX #50
+  BNE E
+Y LDX #0
+  STX 0
+  LDA 2
+  ADC #1
+  STA 2
+  INY
+  CPY #14
+  BNE E
+  LDY #0
+E RTS
+;.END
