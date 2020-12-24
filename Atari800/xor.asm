323af94c054e3ecd5fc8848f92fd3d1547d25a0b
Mariano Dominguez <mariano.dm@gmail.com>
2020-12-23 18:39:24 -0800

    test minimal

--- Atari800/xor.asm
+++ Atari800/xor.asm
@@ -1,35 +1,24 @@
-  org $4000
+  org $2000
   LDA #9 ; gr mode
   JSR $EF9C ;graphics mode
   
   LDX #0
   LDA #0
-  STA 0
-  STA 2
+  STA $0
+  STA $2
   LDY #14 ;pixel lum
   
-L LDA 2 
+L LDA $2 
   STA 84 ;row
-  LDA 0 
+  LDA $0 
   STA 85 ;col low
 
   STY 763 ; color reg
-  JSR X
 C JSR $F1D8 ; call OS PLOT
+  JSR X
   JMP L
 
 X INX
-  STX 0
-  CPX #50
-  BNE E
-Y LDX #0
-  STX 0
-  LDA 2
-  ADC #1
-  STA 2
-  INY
-  CPY #14
-  BNE E
-  LDY #0
+  STX $0
 E RTS
 ;.END
