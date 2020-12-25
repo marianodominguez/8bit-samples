c13072cf9524f48febec79b8150475e15a6f13e4
Mariano Dominguez <mariano.dm@gmail.com>
2020-12-24 18:09:07 -0800

    working asm xor

--- Atari800/xor.asm
+++ Atari800/xor.asm
@@ -1,5 +1,5 @@
   org $4000
-  LDA #9 ; gr mode
+  LDA #9    ;gr mode
   JSR $EF9C ;graphics mode
   
   LDA #0
@@ -13,34 +13,44 @@ L LDA $F1
   LDA $F0
   STA 85 ;col low
   
-  LDY $F2
-  INY
-  STY $F2
-  CPY #14
-  BNE N
-  LDY #0
-  STY $F2
-
+  LDA $F1
+  LDX #2
+  SEC
+d INX
+  SBC #2
+  BCS d
+  STX $f2
+  
+  LDA $F0 ; color = x xor y/2
+  EOR $F2
+  JSR M
+  TAY
+  
 N STY 763   ;color reg
 C JSR $F1D8 ;call OS PLOT
   JSR X
   JMP L
 
-X LDA $F0
-  CLC
-  ADC #1
-  STA $F0
-  CMP #80
+X LDX $F0
+  INX
+  STX $F0
+  CPX #80
   BNE E
-Y LDA #0
-  CLC
-  STA $F0
-  LDA $F1
-  ADC #1
-  STA $F1
-  CMP #192
+Y LDY #0
+  STY $F0
+  LDY $F1
+  INY
+  STY $F1
+  CPY #192
   BNE E
-  LDA #0
-  STA $F1
+  LDY #0
+  STY $F1
 E RTS
+
+M SEC
+i SBC #14
+  BCS i
+  ADC #14
+  RTS
+  
 ;.END
