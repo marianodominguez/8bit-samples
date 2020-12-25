5cbc820e6442c90f18b25b2b79c55def1d155bfc
Mariano Dominguez <mariano.dm@gmail.com>
2020-12-24 16:29:40 -0800

    fixed loop

--- Atari800/xor.asm
+++ Atari800/xor.asm
@@ -1,24 +1,45 @@
-  org $2000
+  org $4000
   LDA #9 ; gr mode
   JSR $EF9C ;graphics mode
   
-  LDX #0
   LDA #0
-  STA $0
-  STA $2
-  LDY #14 ;pixel lum
+  STA $0 ;x
+  STA $2 ;y
+  STA $4 ;color
   
 L LDA $2 
   STA 84 ;row
-  LDA $0 
+  LDA $0
   STA 85 ;col low
+  
+  ;LDA $0 ;c=x xor y
+  ;EOR $1
+  ;TAY
+  LDA $4
+  ADC #1
+  CMP #14
+  BNE N
+  LDA #0
+  STA $4
 
-  STY 763 ; color reg
-C JSR $F1D8 ; call OS PLOT
+N STA 763   ;color reg
+C JSR $F1D8 ;call OS PLOT
   JSR X
   JMP L
 
-X INX
-  STX $0
+X LDA $0
+  ADC #1
+  STA 0
+  CMP #80
+  BNE E
+Y LDA #0
+  STA $0
+  LDA $2
+  ADC #1
+  STA $2
+  CMP #192
+  BNE E
+  LDA #0
+  STA $2
 E RTS
 ;.END
