d63d27d61d505663e1af0b6f1c6290ec8db84094
Mariano Dominguez <mariano.dm@gmail.com>
2020-12-25 23:10:36 -0800

    add wave examples

---
+++ Atari800/message_tw.asm
@@ -0,0 +1,13 @@
+SAVMSC 	=	$58
+MAXLEN 	=	100
+START LDY #0
+  		LDA #0
+LOOP	LDA STRING,Y
+		  CMP #$9B
+		  BEQ PAUSE
+		  STA (SAVMSC),Y
+		  INY
+		  CPY MAXLEN
+		  BNE LOOP
+PAUSE 	JMP PAUSE
+STRING 	.byte $0,$40,"  Hello 6502 World !  ",$40,$9B ; EOL
--- Atari800/print_str.asm
+++ Atari800/print_str.asm
@@ -5,12 +5,12 @@ MAXLEN 	=	100
 start 	ldy #0
   		lda #0
 loop	lda string,Y
-		cmp #$FF
+		cmp #$9B
 		beq pause
 		sta (SAVMSC),Y
 		iny
 		cpy MAXLEN
 		bne loop
 pause 	jmp pause
-string 	.byte $0,$40,"  Hello 6502 World !  ",$40,$FF
- 		.byte "please terminate your strings with $FF",$FF
+string 	.byte $0,$40,"  Hello 6502 World !  ",$40,$9B ; EOL,
+ 		.byte "please terminate your strings with $9B",$9B
