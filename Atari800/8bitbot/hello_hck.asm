26478fa1673db1d6691cb0f89075945cc9ea9d62
Mariano Dominguez <mariano.dm@gmail.com>
2020-12-28 23:59:44 -0800

    added twitter versions

---
+++ Atari800/8bitbot/hello_hck.asm
@@ -0,0 +1,23 @@
+	org $4000
+CM=$0342
+BAL=$0344
+BAH=$0345
+AX1=$034A
+AX2=$034B
+CV=$E456
+
+	LDA #2
+	JSR  $EF9C
+L 	LDA M,Y
+	CMP #$9B
+	BEQ C
+	STA ($58),Y
+	INY
+	JMP L
+C 	LDA $D40B
+	CLC
+	ADC $14
+	STA $D40A 
+	STA $D017
+	JMP C
+M 	.BYTE 0,40,"ello",0,22,21,16,18,0,"world",1,$9B
