6fd9f3b1765afcced62934efb983c18a51336f2f
Mariano Dominguez <mariano.dm@gmail.com>
2023-11-26 20:55:58 -0800

    add diamond and loating point raw implementation.

---
+++ cc65/simple_graph/include/fp_trig.h
@@ -0,0 +1,11 @@
+#include <stdint.h>
+#ifndef TABLE_SIZE
+#define TABLE_SIZE 360
+#endif
+
+#ifndef SCALE_FACTOR
+#define SCALE_FACTOR 32767
+#endif
+
+int16_t f_sin(int degrees);
+int16_t f_cos(int degrees);
---
+++ cc65/simple_graph/src/diamond.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <atari.h>
+#include "atari_draw.h"
+#include <peekpoke.h>
+#include <conio.h>
+#include "fp_trig.h"
+
+void wait_start() {
+    int i;
+    int key=0;
+    while (key!=6) {
+        key = PEEK(0xD01f);
+        for (i=0; i<500; i++);
+    }
+}
+
+int main(void) {
+    long x,y,x1,y1;
+    int ph,th;
+    int fd = _graphics(8+16);
+
+    if (fd == -1) {
+        cputsxy(0,0,"Unable to get graphic mode");
+        exit(1);
+    }
+
+    cursor(0);
+    //printf("Diamond\n");
+
+    // Store fd for screen
+    _setscreen(fd);
+
+    _setcolor(1,1,14);
+    _setcolor(2,4,4);
+    _color(1);
+
+    for(th=0;th<360;th+=20) {
+        x = (long) 70*f_sin(th) / SCALE_FACTOR + 160;
+        y = 96 - (long) 70*f_cos(th) / SCALE_FACTOR ;
+        //printf("%ld,", ((long) 70*f_sin(th) ) / SCALE_FACTOR );
+        for(ph=0;ph<360;ph+=20) {
+            x1 = (long) 70*f_sin(ph)  / SCALE_FACTOR + 160;
+            y1 = 96- (long) 70*f_cos(ph)  / SCALE_FACTOR;
+            _plot(x,y);
+            _drawto(x1,y1);
+        }
+    }
+
+    wait_start();
+
+    return EXIT_SUCCESS;
+}
