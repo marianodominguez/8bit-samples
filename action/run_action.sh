cec51848039890b36d8927a093c37de9a4fd581b
Mariano DM <mariano.dm@gmail.com>
2026-01-01 20:49:41 -0800

    validate tools

--- action/run_action.sh
+++ action/run_action.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
+
+if [ ! $(which xdotool) ]; then
+  echo "xdotool not found, install it before running"
+  exit 1
+fi
+
 WID=$(xdotool search --onlyvisible --class "Atari800" | head -n 1)
 echo "WID: $WID"
+
+if [ -z "$WID" ]; then
+  echo "Atari800 not found"
+  exit 1
+fi
 
 xdotool windowactivate $WID
 xdotool key --delay 100 shift+ctrl+r
