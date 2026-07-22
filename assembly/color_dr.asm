a5f5efcfe26871a09f00b1c2c3ba0b95274c5b44
Mariano DM <mariano.dm@gmail.com>
2026-07-21 20:05:40 -0700

    add dist script for dino

--- assembly/color_dr.asm
+++ assembly/color_dr.asm
@@ -24,7 +24,7 @@ Y   	=  $F1
 STOCOL 	=  $CD ;store color
 
   org $4000
-  LDA #7 ; gr mode
+  LDA #7+16 ; gr mode
   ;JSR $EF9C ;graphics mode
   JSR MODE
 
@@ -39,71 +39,95 @@ STOCOL 	=  $CD ;store color
   LDY #0
   LDX #0
 
-loop LDA Y 
-  LDX STOCOL
-  INX
-  CPX #4
-  BNE line
-  LDX #0
-line 
-  STX STOCOL
-  LDX #10 ;x
-  LDY #10 ;y
+loop
   LDA STOCOL
+  ADC #1
+  CMP #4
+  BNE line
+  LDA #0
+  STA STOCOL
+line 
+  STA STOCOL
+  LDA #0
+  LDX #0
+  LDY #0
   JSR PLOT ; set cursor
+  LDX X
   LDY Y
   INY
   INY
+  STY Y
+
   CPY #90
   BNE dr
   LDY #0
-dr
   STY Y
-  LDX X
+  LDA #0
+  JMP LOOP
+dr
   JSR DRAWTO
   JMP loop
 
-PLOT
-   JSR $F1D8 ;call OS PLOT
-   JSR POSITN    ; To store info
-   LDX #$60      ; For the screen
-   LDA #0        ; Special case of
-   STA ICBLL,X   ;  I/O using the
-   STA ICBLH,X   ;  accumulator
-   LDA STOCOL    ; Get COLOR to use
-
-
-POSITN
-  	STX COLCRS    ; Low byte of X
-  	STY ROWCRS    ; Y position
-  	RTS           ; All done
-
-DRAWTO
-	 JSR POSITN    ; To store info
-	 LDA STOCOL       ; Get COLOR
-	 STA ATACHR    ; Keep CIO happy
-	 LDX #$60      ; The screen again
-	 LDA #$11      ; For DRAWTO
-	 STA ICCOM,X   ; Command byte
-	 LDA #$C       ; As in XIO
-	 STA ICAX1,X   ; Auxiliary 1
-	 LDA #0        ; Clear
-	 STA ICAX2,X   ; Auxiliary 2
-	 JSR CIOV      ; Draw the line
-	 RTS           ; All done
-
 MODE
-   	PHA
-	LDX #$60      ; The screen again
-   	LDA #NAME&255 ; Name is "S:"
-   	STA ICBAL,X   ; Low byte
-   	LDA #NAME/256 ; High byte
-   	STA ICBAH,X
-   	PLA
-   	JSR $EF9C
-   	LDA #0
-   	STA COLCRS+1   ;this is mode <8 
-	RTS
+       PHA           ; Store on stack
+       LDX #$60      ; IOCB6 for screen
+       LDA #$C       ; CLOSE command
+       STA ICCOM,X   ; in command byte
+       JSR CIOV      ; Do the CLOSE
+       LDX #$60      ; The screen again
+       LDA #3        ; OPEN command
+       STA ICCOM,X   ; in command byte
+       LDA #NAME&255 ; Name is "S:"
+       STA ICBAL,X   ; Low byte
+       LDA #NAME/256 ; High byte
+       STA ICBAH,X
+       PLA           ; Get GRAPHICS n
+       STA ICAX2,X   ; Graphics mode
+       AND #$F0      ; Get high 4 bits
+       EOR #$10      ; Flip high bit
+       ORA #$C       ; Read or write
+       STA ICAX1,X   ; n+16, n+32 etc.
+       JSR CIOV      ; Setup GRAPHICS n
+       RTS           ; All done
+
+; ******************************
+; The POSITION command
+; ******************************
+POSITN
+       STX COLCRS    ; Low byte of X
+       STA COLCRS+1  ; High byte of X
+       STY ROWCRS    ; Y position
+       RTS           ; All done
+; ******************************
+; The PLOT command
+; ******************************
+PLOT
+       JSR POSITN    ; To store info
+       LDX #$60      ; For the screen
+       LDA #$B       ; Put record
+       STA ICCOM,X   ; Command byte
+       LDA #0        ; Special case of
+       STA ICBLL,X   ;  I/O using the
+       STA ICBLH,X   ;  accumulator
+       LDA STOCOL    ; Get COLOR to use
+       JSR CIOV      ; Plot the point
+       RTS           ; All done
+; ******************************
+; The DRAWTO command
+; ******************************
+DRAWTO
+       JSR POSITN    ; To store info
+       LDA STOCOL    ; Get COLOR
+       STA ATACHR    ; Keep CIO happy
+       LDX #$60      ; The screen again
+       LDA #$11      ; For DRAWTO
+       STA ICCOM,X   ; Command byte
+       LDA #$C       ; As in XIO
+       STA ICAX1,X   ; Auxiliary 1
+       LDA #0        ; Clear
+       STA ICAX2,X   ; Auxiliary 2
+       JSR CIOV      ; Draw the line
+       RTS           ; All done
  
 NAME   .BY "S:",$9B
 ;.END
