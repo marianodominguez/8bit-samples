8f9261d894b3181bd149ee1bed50ec224aeaeb5d
Mariano DM <mariano.dm@gmail.com>
2026-08-26 09:29:15 -0700

    fix diamond to init w/o start key

--- cc65/simple_graph/src/diamond.c
+++ cc65/simple_graph/src/diamond.c
@@ -6,25 +6,30 @@
 #include <conio.h>
 #include "fp_trig.h"
 
-void wait_start() {
+void wait_start()
+{
     int i;
-    int key=0;
-    while (key!=6) {
+    int key = 0;
+    while (key != 6)
+    {
         key = PEEK(0xD01f);
-        for (i=0; i<500; i++);
+        for (i = 0; i < 500; i++)
+            ;
     }
 }
 
-int main(void) {
-    long x,y,x1,y1;
-    int ph,th;
+int main(void)
+{
+    long x, y, x1, y1;
+    int ph, th;
     int fd = _graphics(8);
     unsigned int r = 80;
-    if (fd == -1) {
-        cputsxy(0,0,"Unable to get graphic mode");
+    int i = 0;
+    if (fd == -1)
+    {
+        cputsxy(0, 0, "Unable to get graphic mode");
         exit(1);
     }
-    wait_start();
 
     cursor(0);
     printf("Diamond\n");
@@ -32,20 +37,23 @@ int main(void) {
     // Store fd for screen
     _setscreen(fd);
 
-    _setcolor(1,1,14);
-    _setcolor(2,5,4);
+    _setcolor(1, 1, 14);
+    _setcolor(2, 5, 4);
     _color(1);
 
-    for(th=0;th<360;th+=20) {
-        x = (long) (r+10)*f_sin(th) / SCALE_FACTOR + 160;
-        y = 96 - (long) r*f_cos(th) / SCALE_FACTOR -15;
+    for (th = 0; th < 360; th += 20)
+    {
+        x = (long)(r + 10) * f_sin(th) / SCALE_FACTOR + 160;
+        y = 96 - (long)r * f_cos(th) / SCALE_FACTOR - 15;
 
-        for(ph=0;ph<360;ph+=20) {
-            x1 = (long) (r+10)*f_sin(ph)  / SCALE_FACTOR + 160;
-            y1 = 96- (long) r*f_cos(ph)  / SCALE_FACTOR -15;
-            _plot(x,y);
-            _drawto(x1,y1);
+        for (ph = i * 20; ph < 360; ph += 20)
+        {
+            x1 = (long)(r + 10) * f_sin(ph) / SCALE_FACTOR + 160;
+            y1 = 96 - (long)r * f_cos(ph) / SCALE_FACTOR - 15;
+            _plot(x, y);
+            _drawto(x1, y1);
         }
+        i++;
     }
 
     wait_start();
